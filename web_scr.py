5d41c3f0377d0e0a9d291fca8a380104116a6b90
qubitAI <osmanmorales@ymail.com>
2018-10-12 21:22:22 -0400

    Update web_scr.py

--- web_scr.py
+++ web_scr.py
@@ -9,7 +9,6 @@ import json
 def get_soup(url,header):
     return BeautifulSoup(urllib3.urlopen(urllib3.Request(url,headers=header)),'html.parser')
 
-
 query = "trump"# you can change the query for the image  here
 image_type="ActiOn"
 query= query.split()
@@ -21,7 +20,6 @@ DIR="Pictures"
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
 }
 soup = get_soup(url,header)
-
 
 ActualImages=[]# contains the link for Large original images, type of  image
 for a in soup.find_all("div",{"class":"rg_meta"}):
@@ -48,7 +46,6 @@ for i , (img , Type) in enumerate( ActualImages):
             f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+".jpg"), 'wb')
         else :
             f = open(os.path.join(DIR , image_type + "_"+ str(cntr)+"."+Type), 'wb')
-
 
         f.write(raw_img)
         f.close()
