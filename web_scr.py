64546b275e49f337b40dc05c79e09f536e1fdbc3
qubitAI <osmanmorales@ymail.com>
2018-10-24 23:31:21 -0400

    Update web_scr.py

--- web_scr.py
+++ web_scr.py
@@ -18,6 +18,7 @@ url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
 print (url)
 #add the directory for your image here
 DIR="Pictures"
+#notice version of Mozilla and Chrome being used for this work
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"
 }
 soup = get_soup(url,header)
